c49c6d96df64865bd5bdc94ca19a0a2f7936d4b3
Veffica <jessyca.1991@gmail.com>
2024-01-02 15:39:01 -0300

    Update Box.js

--- Box.js
+++ Box.js
@@ -9,6 +9,8 @@ class Box{
 		this.game = game;
         this.arrayLength = length;
 
+        this.cube;
+
 		this.loadBox();
 	}
 
@@ -23,22 +25,23 @@ class Box{
 
         this.geometry = new THREE.BoxGeometry( 1, 1, 1 ); 
         this.material = new THREE.MeshBasicMaterial( {color: color} ); 
-        cube = new THREE.Mesh( this.geometry, this.material ); 
+        this.cube = new THREE.Mesh( this.geometry, this.material ); 
         
-        cube.position.x = 0;
-        cube.position.y = 0.5;
-        cube.position.z = valz;
-        console.log(this.arrayLength+ " : " +cube.position.z+ " color: "+ color);
-        cube.rotation.x = 0;
-        cube.rotation.y = 0;
+        this.cube.position.x = 0;
+        this.cube.position.y = 0.5;
+        this.cube.position.z = valz;
+        console.log(this.arrayLength+ " : " +this.cube.position.z+ " color: "+ color);
+        this.cube.rotation.x = 0;
+        this.cube.rotation.y = 0;
 
-        this.game.scene.add( cube );
+        this.game.scene.add( this.cube );
        
     }
 
     update(){
-        cube.position.z = cube.position.z -0.02;
-        cube.position.x = cube.position.x - 0.02;
+        this.cube.position.z = this.cube.position.z -0.02;
+        this.cube.position.x = this.cube.position.x + 0.02;
+        console.log(this.arrayLength+ " : " +this.cube.position.z+ " color: "+ color);
     }
 }
 
